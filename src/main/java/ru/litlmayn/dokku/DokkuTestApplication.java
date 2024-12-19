28710e45c0d21f0753f1a21f10096e0a0358ea3e
Вадим Крамаренко <litlmayn@mail.ru>
2024-12-19 17:43:13 +0300

    initial project

---
+++ src/main/java/ru/litlmayn/dokku/DokkuTestApplication.java
@@ -0,0 +1,13 @@
+package ru.litlmayn.dokku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokkuTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DokkuTestApplication.class, args);
+    }
+
+}
